dba737adb6c038ea3249f76517fba19f3b838576
Michel du Croix <mlcroix@avans.nl>
2017-01-11 04:01:28 +0100

    deck.h

--- Machiavelli/Machiavelli/Deck.h
+++ Machiavelli/Machiavelli/Deck.h
@@ -1,8 +1,22 @@
 #pragma once
+//kan queue en memory niet gebruiken, hebben conflicten met de std queue's in de main
+//#include <queue>
+//#include <memory>
+#include <vector>
+#include <fstream>
+#include <Iostream>
+#include <string>
+#include "Card.h"
+
 class Deck
 {
 public:
 	Deck();
 	~Deck();
+	//void addCard(std::unique_ptr<Card> c);
+	//std::unique_ptr<Card> drawCard();
+	void fillDeck();
+private:
+	//std::queue<std::unique_ptr<Card>> _deck;
 };
 
