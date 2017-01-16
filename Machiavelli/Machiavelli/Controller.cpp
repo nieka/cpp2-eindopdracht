53e983cb2714f273c8084b7dd39f31b5bf919392
Michel du Croix <mlcroix@avans.nl>
2017-01-16 21:55:53 +0100

    name check
    als speler 2 dezelfde naam als speler 1 heeft dan krijgt speler 2 een
    "2" achter zijn naam.

--- Machiavelli/Machiavelli/Controller.cpp
+++ Machiavelli/Machiavelli/Controller.cpp
@@ -17,8 +17,14 @@ void Controller::handleCommand(ClientCommand command)
 	if (auto clientInfo = command.get_client_info().lock()) {
 		if (!started) {
 			if (command.get_cmd() == "join") {
+
+				if (players.size() > 0 && clientInfo->get_player().get_name() == players[0].get_name())
+				{
+					clientInfo->get_player().set_name(clientInfo->get_player().get_name() + "2");
+					clientInfo->get_socket().write("Er is al een speler met dezlefde naam, je naam wordt aangepast naar: " + clientInfo->get_player().get_name() + "\r\n");
+				}
+
 				players.push_back(clientInfo->get_player());
-				//playerSockets.insert({clientInfo->get_player().get_name(), std::move(clientInfo->get_socket()) });
 
 				std::shared_ptr<ClientInfo> cinfo { clientInfo };
 				playerSockets.insert({ clientInfo->get_player().get_name(), cinfo });
