ec059fe6634944a4c84560d0b390bdfa2ff40651
Niek Hoffmans <niek.f.j.hoffmans@gmail.com>
2017-01-19 19:02:59 +0100

    koopman added

--- Machiavelli/Machiavelli/Koopman.cpp
+++ Machiavelli/Machiavelli/Koopman.cpp
@@ -13,9 +13,27 @@ Koopman::~Koopman()
 
 void Koopman::karakterInfo(Controller & controller, GameController & gcon)
 {
+	controller.printToPlayer("type 1 om de goudstukken te krijgen die je als koopman verdient", gcon.getCurrentPlayer().get_name());
 }
 
 bool Koopman::play(int input, Controller & controller, GameController & gcon)
 {
+	if (input == 1) {
+		int goudstukken = 1;
+		//geef de karakter voor elk blauw gebouw een goudstuk
+		std::for_each(gcon.getCurrentPlayer().getGebouwdeKaarten().begin(), gcon.getCurrentPlayer().getGebouwdeKaarten().end(), [&](std::shared_ptr<Card> bouwkaart)
+		{ 
+			if (bouwkaart->getColor() == "blauw") {
+				goudstukken++;
+			}
+		});
+
+		gcon.getCurrentPlayer().addGoudStukken(goudstukken);
+		controller.printToPlayer("Je hebt er " + std::to_string(goudstukken) + " bij gekregen en hebt nu " + std::to_string(gcon.getCurrentPlayer().getGoudstukken()), gcon.getCurrentPlayer().get_name());
+	}
+	else {
+		controller.printToPlayer("Je hebt geen goudstukken gekregen", gcon.getCurrentPlayer().get_name());
+	}
+	
 	return true;
 }
