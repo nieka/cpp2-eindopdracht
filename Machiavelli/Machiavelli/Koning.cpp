6d44a7591e5c8a09ec1050c51efc496353ec8799
Niek Hoffmans <niek.f.j.hoffmans@gmail.com>
2017-01-20 12:08:01 +0100

    koing added

--- Machiavelli/Machiavelli/Koning.cpp
+++ Machiavelli/Machiavelli/Koning.cpp
@@ -13,9 +13,27 @@ Koning::~Koning()
 
 void Koning::karakterInfo(Controller & controller, GameController & gcon)
 {
+	controller.printToPlayer("type 1 om de goudstukken te krijgen die je als koning verdient", gcon.getCurrentPlayer().get_name());
 }
 
 bool Koning::play(int input, Controller & controller, GameController & gcon)
 {
+	if (input == 1) {
+		int goudstukken = 1;
+		//geef de karakter voor elk blauw gebouw een goudstuk
+		for each (auto bouwkaart in gcon.getCurrentPlayer().getGebouwdeKaarten())
+		{
+			if (bouwkaart->getColor() == "geel") {
+				goudstukken++;
+			}
+		}
+
+		gcon.getCurrentPlayer().addGoudStukken(goudstukken);
+		controller.printToPlayer("Je hebt er " + std::to_string(goudstukken) + " bij gekregen en hebt nu " + std::to_string(gcon.getCurrentPlayer().getGoudstukken()), gcon.getCurrentPlayer().get_name());
+	}
+	else {
+		controller.printToPlayer("Je hebt geen goudstukken gekregen", gcon.getCurrentPlayer().get_name());
+	}
+
 	return true;
 }
